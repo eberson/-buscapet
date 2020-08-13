9d74d174c9e444515ce85c6973a2b07e27318f63
Eberson Oliveira <eberson.olivera@gmail.com>
2020-08-13 20:46:11 -0300

    adicionada uma atividade para lidar com a camera

--- app/src/main/java/br/com/etecmatao/buscapet/PetLostRegister.kt
+++ app/src/main/java/br/com/etecmatao/buscapet/PetLostRegister.kt
@@ -1,10 +1,9 @@
 package br.com.etecmatao.buscapet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import br.com.etecmatao.buscapet.fragments.PetLostConfirmPictureFragment
@@ -13,25 +12,39 @@ import br.com.etecmatao.buscapet.fragments.PetLostSaveFragment
 class PetLostRegister : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
+    private lateinit var adapter: PetLostPageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_lost_register)
 
+        adapter = PetLostPageAdapter(this)
+
         viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = PetLostPageAdapter(this)
+        viewPager.adapter = adapter
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        adapter.fragmentByPosition(viewPager.currentItem).onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
     }
 
     private inner class PetLostPageAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
+        private val fragments : MutableList<Fragment> = mutableListOf(
+            PetLostConfirmPictureFragment(),
+            PetLostSaveFragment(),
+            PetLostSaveFragment()
+        )
+
+        fun fragmentByPosition(position: Int): Fragment = fragments[position]
         override fun getItemCount(): Int = 3
-
-        override fun createFragment(position: Int): Fragment {
-            if (position == 2){
-                return PetLostSaveFragment()
-            }
-
-            return PetLostConfirmPictureFragment()
-        }
-
+        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 }
---
+++ app/src/main/java/br/com/etecmatao/buscapet/model/PetLost.kt
@@ -0,0 +1,7 @@
+package br.com.etecmatao.buscapet.model
+
+data class PetLost(
+    var id: Long? = null,
+    var title: String,
+    var description: String
+)
